651da10d1a1010773ed574bf00dd4b55950e1b5e
Miftahunmohin <miftahunmohin@yahoo.com>
2014-09-28 14:44:06 +0600

    Added

---
+++ tranzasion/tranzasion/Account.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tranzasion
+{
+    class Account
+    {
+      private string number;
+      private string name;
+      private double balance=0;
+
+      #region old private data access use 
+      //public void SetNumber(string number)
+      //  {
+      //      this.number = number;
+      //  }
+
+      //  public string GetNumber()
+      //  {
+      //      return number;
+      //  }
+      //  public void SetName(string name)
+      //  {
+      //      this.name = name;
+      //  }
+
+      //  public string GetName()
+      //  {
+      //      return name;
+      //  }
+      //  public void SetBlance(double blance)
+      //  {
+      //      this.balance = blance;
+      //  }
+
+      //  public double GetBlance()
+      //  {
+      //      return balance;
+      //  }
+      #endregion
+
+      public string Number
+      {
+          get { return number; }
+          set { number = value; }
+      }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            
+        }
+
+        public void Deposit(double inAmount)
+        {
+            balance += inAmount;
+        }
+
+        public void WithDorow(double outAmount)
+        {
+            balance -= outAmount;
+        }
+
+       
+    }
+}
---
+++ tranzasion/tranzasion/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace tranzasion
+{
+    public partial class Form1 : Form
+    {
+        private Account account;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            account = new Account(); 
+            account.Number =  ( accountNumberTextBox.Text);
+            account.Name= ( customarNameTextBox.Text);
+            MessageBox.Show("Account created!!!!!");
+            accountNumberTextBox.Text=string.Empty;
+            customarNameTextBox.Text=string.Empty;
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+         //   MessageBox.Show(account.Report());
+            string mess = account.Name + " your account is : " + account.Number + " it's balance is : " + account.Balance;
+            MessageBox.Show(mess);
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            if (account!=null)
+            {
+                account.Deposit(Convert.ToDouble(DepositTextBox.Text));
+                DepositTextBox.Text = string.Empty;
+                MessageBox.Show(@"succesfully Deposit!!!");
+            }
+            else
+            {
+                MessageBox.Show(@"please create a account first!!!");
+            }
+        }
+
+        private void withdrowButton_Click(object sender, EventArgs e)
+        {
+            if (account != null)
+            {
+                account.WithDorow(Convert.ToDouble(withdrowTextBox.Text));
+                withdrowTextBox.Text = string.Empty;
+                MessageBox.Show(@"succesfully WithDorow!!!");
+            }
+            else
+            {
+                MessageBox.Show(@"please create a account first!!!");
+            }
+        }
+    }
+}
